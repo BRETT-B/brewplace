76da8092a36157bd26f9c387b7e0ec3b5107c0a2
Brett D Burdick <burdickbrett@outlook.com>
2016-08-19 22:32:06 -0400

    init commit

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ brewApp.controller('brewController', function($scope, $http){
 	$scope.breweries = [];
 
 	$scope.breweriesByCity = function(city){
-		var cityURL = 'http://api.brewerydb.com/v2/locations?key=b96c391074e29a0289645cf3874e6831&locality=' + city;
+		var cityURL = 'http://api.brewerydb.com/v2/locations?locality='+encodeURI(city)+'&key=b96c391074e29a0289645cf3874e6831';
 		console.log(cityURL);
 		$http({
 			method: 'GET',
