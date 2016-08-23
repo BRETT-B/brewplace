ec0e70cc197c8f001c8bb06232245d58117c8ca5
Brett D Burdick <burdickbrett@outlook.com>
2016-08-22 21:07:21 -0400

    master scripts was behind

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
-var brewApp = angular.module('brewApp', []);
+var brewApp = angular.module('brewApp', ['ui.bootstrap']);
 
-brewApp.controller('brewController', function($scope, $http){
+brewApp.controller('brewController', function($scope){
 
-	$scope.breweries = [];
+	$scope.breweries = atlBreweries;
 
+<<<<<<< HEAD
+});
+
+
+	// $scope.breweriesByCity = function(city){
+	// 	var cityURL = 'http://api.brewerydb.com/v2/locations?key=b96c391074e29a0289645cf3874e6831&locality=' + city;
+	// 	console.log(cityURL);
+	// 	$http({
+	// 		method: 'GET',
+	// 		url: cityURL
+	// 	}).then(
+	// 		function successFunction(cityData){
+	// 			$scope.breweries = cityData[0].data;
+	// 			console.log($scope.breweries);
+	// 		},function failureFunction(cityData){
+	// 			console.log(cityData);
+	// 	});
+	// }
+=======
 	$scope.breweriesByCity = function(city){
 		var cityURL = 'http://api.brewerydb.com/v2/locations?locality='+encodeURI(city)+'&key=b96c391074e29a0289645cf3874e6831';
 		console.log(cityURL);
@@ -20,3 +39,4 @@ brewApp.controller('brewController', function($scope, $http){
 	}
 
 });
+>>>>>>> 76da8092a36157bd26f9c387b7e0ec3b5107c0a2
