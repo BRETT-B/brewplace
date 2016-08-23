93b34844fd488f46fd87a0884dc2a470d6dd8455
dwesleyprice <dwesleyprice@gmail.com>
2016-08-23 14:21:27 -0400

    menuController error fix and index.html typo fix

--- js/menuController.js
+++ js/menuController.js
@@ -2,35 +2,3 @@ brewApp.controller('menuController', function($scope){
 	$scope.breweries = atlBreweries;
 });
 
-	// $scope.breweriesByCity = function(city){
-	// 	var cityURL = 'http://api.brewerydb.com/v2/locations?key=b96c391074e29a0289645cf3874e6831&locality=' + city;
-	// 	console.log(cityURL);
-	// 	$http({
-	// 		method: 'GET',
-	// 		url: cityURL
-	// 	}).then(
-	// 		function successFunction(cityData){
-	// 			$scope.breweries = cityData[0].data;
-	// 			console.log($scope.breweries);
-	// 		},function failureFunction(cityData){
-	// 			console.log(cityData);
-	// 	});
-	// }
-
-	$scope.breweriesByCity = function(city){
-		var cityURL = 'http://api.brewerydb.com/v2/locations?locality='+encodeURI(city)+'&key=b96c391074e29a0289645cf3874e6831';
-		console.log(cityURL);
-		$http({
-			method: 'GET',
-			url: cityURL
-		}).then(
-			function successFunction(cityData){
-				console.log(cityData.data);
-				$scope.breweries = cityData;
-			},function failureFunction(cityData){
-				console.log(cityData);
-		});
-	
-
-};
-
