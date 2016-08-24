ba524be0c2e7f3601c725c4aeb18b4a4e7530aad
Keith Moore <keith.m4@gmail.com>
2016-08-24 15:43:16 -0400

    added stuff to modals

--- app/components/modal/modalController.js
+++ app/components/modal/modalController.js
@@ -2,6 +2,12 @@ brewApp.controller('modalController', function ($scope, $uibModalInstance, param
 
 	$scope.options = params.searchTerm;
 
+    // $scope.imgLoadedEvents = {
+    //     fail: function(instance) {
+    //         console.log("failed: " + instance);
+    //     }
+    // };
+
 	$scope.close = function() {
 		$uibModalInstance.close();
 	};
