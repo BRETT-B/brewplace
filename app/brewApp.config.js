1164b8d0eaf5cf6f5854b2d8648ad8fb973fb435
Keith Moore <keith.m4@gmail.com>
2016-08-24 16:43:50 -0400

    added beer101 route and modal descriptions; linked new controller and beer101 json

--- app/brewApp.config.js
+++ app/brewApp.config.js
@@ -7,6 +7,10 @@ brewApp.config(function($routeProvider) {
 		templateUrl: 'app/components/brewery/breweryView.html',
 		controller: 'breweryController'
 	})
+	.when('/beer101' {
+		templateUrl: 'app/components/beer101/beer101.html',
+		controller: 'beer101Controller'
+	})
 	.otherwise({
 		redirectTo: '/'
 	});
