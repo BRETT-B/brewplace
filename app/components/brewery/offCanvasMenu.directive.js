8afe18a1f0ecce30a31c06a44f29f0c9c9024e8c
dwesleyprice <dwesleyprice@gmail.com>
2016-08-26 13:09:27 -0400

    comments on directive and beer101.html

--- app/components/brewery/offCanvasMenu.directive.js
+++ app/components/brewery/offCanvasMenu.directive.js
@@ -1,11 +1,15 @@
 brewApp.directive('offCanvasMenu', function () {
     return {
+        // this directive returns an attribute
         restrict: 'A',
         replace: false,
+        // using link instead of a controller to manipulate the DOM
         link: function (scope, element) {
+            // Creates a "switch" for the menu to toggle on an off:
             scope.isMenuOpen = false;
             scope.toggleMenu = function() {
                 scope.isMenuOpen = !scope.isMenuOpen;
+                    // condition to remove the array from the view
                 if(!scope.isMenuOpen){ 
                     scope.beers = [];
                     scope.chosenBrewery = "";    
